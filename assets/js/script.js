f32e5607a4f0e049a7e3a745bdf0e8048c25cf4c
Nghiem Ngoc Quynh <nghiemquynh1312@gmail.com>
2021-05-19 00:29:53 +0700

    create multiple page html

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,20 @@ $(".carousel").swipe({
 //     header.classList.remove("sticky");
 //   }
 // }
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
